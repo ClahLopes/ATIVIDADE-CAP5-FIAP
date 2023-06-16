58041d24aa81a6fc52a70ee3e7392c7cd66d6cd9
Claudia dos Santos Lopes <lopes.claudia.csl@gmail.com>
2023-06-15 22:09:37 -0300

    component menu

--- my-project/src/Area Investimento/Menu.jsx
+++ my-project/src/Area Investimento/Menu.jsx
@@ -1,11 +1,11 @@
 import { List } from '@phosphor-icons/react';
 import React from 'react';
 
-function Menu() {
+function Menu({props}) {
   return (
     <div className='w-[375px] flex items-center bg-[#3F3F3F] text-[#FFFFF] '>
         <List className='text-[#F5F5F5] mr-12 ml-2 text-2xl' />
-        <h1 className='text-[#F5F5F5] max-md:text-center p-5 text-xl'>ÁREA DO INVESTIDOR</h1>
+        <h1 className='text-[#F5F5F5] max-md:text-center p-5 text-xl'>{props}</h1>
     </div>
   )
 }
