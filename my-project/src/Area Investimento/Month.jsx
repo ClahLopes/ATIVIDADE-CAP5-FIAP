13c561c5952572d4ca04dd9e9e3d1123e5be0505
Claudia dos Santos Lopes <lopes.claudia.csl@gmail.com>
2023-06-15 22:09:19 -0300

    component month

--- my-project/src/Area Investimento/Month.jsx
+++ my-project/src/Area Investimento/Month.jsx
@@ -1,13 +1,14 @@
 import { CaretRight } from '@phosphor-icons/react'
 import React from 'react'
 
-function Month() {
+function Month({title1 , title2, title3, title4, title5}) {
     return (
         <div className='w-[375px] flex items-center justify-center bg-[#5C5C5C] text-[#FFFFF] '>
-            <h1 className='text-[#F5F5F5] text-center p-2 text-sm font-bold'>Poupança</h1>
-            <h1 className='text-[#F5F5F5] text-center p-2 text-sm font-bold'>Renda Fixa</h1>
-            <h1 className='text-[#F5F5F5] text-center p-2 text-sm font-bold'>Tesouso Direto</h1>
-            <h1 className='text-[#F5F5F5] text-center p-2 text-sm font-bold'>Ações</h1>
+            <h1 className='text-[#F5F5F5] text-center p-2 text-sm font-bold'>{title1}</h1>
+            <h1 className='text-[#F5F5F5] text-center p-2 text-sm font-bold'>{title2}</h1>
+            <h1 className='text-[#F5F5F5] text-center p-2 text-sm font-bold'>{title3}</h1>
+            <h1 className='text-[#F5F5F5] text-center p-2 text-sm font-bold'>{title4}</h1>
+            <h1 className='text-[#F5F5F5] text-center p-2 text-sm font-bold'>{title5}</h1>
             <CaretRight className='text-[#F5F5F5] ml-4' />
         </div>
 
